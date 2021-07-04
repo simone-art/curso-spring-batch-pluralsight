0fdecd8cefab95a082e184bb21216cf8f52889e6
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-04 12:22:36 -0300

    First Commit

---
+++ demo-spring-batch-course/src/main/java/com/example/demospringbatchcourse/DemoSpringBatchCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatchcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBatchCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBatchCourseApplication.class, args);
+	}
+
+}
---
+++ demo-spring-batch-course/src/test/java/com/example/demospringbatchcourse/DemoSpringBatchCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatchcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBatchCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
